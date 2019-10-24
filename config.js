5b13c8a763d65f8207c8830f1f4f69d4dc9e6d5d
manas2297 <yadavmanas22@gmail.com>
2019-10-24 11:18:41 +0530

    variables

--- config.js
+++ config.js
@@ -6,11 +6,8 @@ exports.userPass = process.env.USER_PASS;
 exports.auth = {
   type: "oauth2",
   user: "yadavmanas22@gmail.com",
-  clientId:
-    "893885968091-2jfhv45llqkqcfpfe17su9v2mh4uh01l.apps.googleusercontent.com",
-  clientSecret: "ss-5pozaOoxMSYtPbrvwkuMw",
-  refreshToken:
-    "1//04sSMHXdO4Vr_CgYIARAAGAQSNwF-L9IrJIN9dN5n-WZBwPefvEIWc9hqZqqshlbjztcv4YocFmO0NxijHjxDnNZ3Ine3JPWKnDA",
-  accessToken:
-    "ya29.Il-iBxUXnGOR0u3WcqzFmIe2VwhrcCaMdQg-BbE-BTGl5vlv9jPFbOzQNrtN_ZYm7IHG6eeWvZctkAa0jeR9DstI_TTdrJUoUKKQPC4nUA_bqvU4zYqn-sWFw2YwlPkdQw"
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN,
+  accessToken: process.env.ACCESS_TOKEN
 };
