0147269ef6d504c56f1a02a3c2967943d5792c4b
Divyanshi Singh <divyanshipilot@gmail.com>
2020-10-01 22:37:40 +0530

    modified code

---
+++ client/src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './contact.css';
+import Heading from 'react-bulma-components/lib/components/heading';
+
+function Contact() {
+    return (
+        <div className="contact">
+            <Heading>
+                Contact Me
+                {/* name, email, text area, send */}
+            </Heading>
+            
+        </div>
+    )
+}
+
+export default Contact
