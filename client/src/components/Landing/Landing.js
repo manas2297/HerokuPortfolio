00c3975b6ca158745cfc3c69ede60034623b49d5
manas2297 <yadavmanas22@gmail.com>
2021-02-23 23:18:35 +0530

    v2.0

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -9,12 +9,8 @@ import Footer from "../Footer";
 import Header from '../Header'
 import Skills from "../Skills";
 import Projects from "../Projects";
-<<<<<<< HEAD
 import Contact from "components/Contact";
 import ContactSide from "components/ContactSide";
-=======
-import Contact from "components/Contact/Contact";
->>>>>>> beb42b4653458ae7e94f10ff0b8036966f0d6884
 const About = React.lazy(() => import('../About'));
 class Landing extends React.Component {
   render() {
